436e5198e5a0124256df4a92840cdcd06e2a305a
Gautam Sharma <gautam2586272@gmail.com>
2025-01-03 23:41:17 +0530

    add login controller, profile controller, user authenticate middleware

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import * as userService from '../services/user.service.js'
+import userModel from '../models/user.model.js';
 import {validationResult} from 'express-validator'
 
 export const createUserController = async(req, res) => {
@@ -13,5 +14,44 @@ export const createUserController = async(req, res) => {
     }
     catch(error) {
         res.status(400).send(error.message);
+    }
+}
+
+export const loginController = async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    try {
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email}).select('+password');
+        if(!user) {
+            return req.status(401).json({
+                errors: 'Invalid credetials'
+            })
+        }
+        const isMatch = await user.isValidPassword(password);
+        if(!isMatch) {
+            return req.status(401).json({
+                errors: 'Invalid credetials'
+            })
+        }
+        const token = await user.generateJWT();
+        delete user._doc.password;  
+        res.status(200).json({user, token});
+    }
+    catch(error) {
+        res.status(400).send(error.message);
+    }
+}
+
+export const profileController = async(req, res) => {
+    try {
+        res.status(200).json({
+            user: req.user
+        })
+    }
+    catch(error) {
+        res.status(400).send(error.message)
     }
 }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,19 @@
 import {Router} from  'express';
 import * as userController from "../controllers/user.controller.js"
 import {body} from 'express-validator'
-
+import * as authMiddleware from "../middlewares/auth.middleware.js"
 const router = Router();
 
 router.post('/register',
     body('email').isEmail().withMessage('Email must be a valid email address'),
     body('password').isLength({min: 6}).withMessage('Password must be atlest 6 characters long'),
     userController.createUserController)
+
+    router.post('/login',
+    body('email').isEmail().withMessage('Email must be a valid email address'),
+    body('password').isLength({min: 6}).withMessage('Password must be atlest 6 characters long'),
+    userController.loginController)
+
+    router.get('/profile', authMiddleware.authUser, userController.profileController)
 
 export default router;
